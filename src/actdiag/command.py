9dc9089e0d89d5f76997914d6e5b95bc9fd8bbcd
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-10-11 10:57:19 +0900

    * Remove unused option: -P

--- src/actdiag/command.py
+++ src/actdiag/command.py
@@ -21,7 +21,6 @@ from optparse import OptionParser
 import actdiag
 import DiagramDraw
 import diagparser
-from blockdiag import utils
 from blockdiag.command import detectfont
 from builder import ScreenNodeBuilder
 
@@ -38,8 +37,6 @@ def parse_option():
                  help='write diagram to FILE', metavar='FILE')
     p.add_option('-f', '--font', default=[], action='append',
                  help='use FONT to draw diagram', metavar='FONT')
-    p.add_option('-P', '--pdb', dest='pdb', action='store_true', default=False,
-                 help='Drop into debugger on exception')
     p.add_option('-s', '--separate', action='store_true',
                  help='Separate diagram images for each group (SVG only)')
     p.add_option('-T', dest='type', default='PNG',
@@ -96,9 +93,6 @@ def main():
         outfile = 'output.' + options.type.lower()
     else:
         outfile = re.sub('\..*', '', infile) + '.' + options.type.lower()
-
-    if options.pdb:
-        sys.excepthook = utils.postmortem
 
     fontpath = detectfont(options)
 
