8bf73f6626c07fd811bac0f5ddfe11a268f6416c
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2012-11-05 17:45:19 +0900

    Fix pep8 violation

--- src/actdiag/builder.py
+++ src/actdiag/builder.py
@@ -14,7 +14,8 @@
 #  limitations under the License.
 
 from actdiag import parser
-from actdiag.elements import Diagram, DiagramNode, DiagramEdge, NodeGroup, unquote
+from actdiag.elements import (Diagram, DiagramNode, DiagramEdge,
+                              NodeGroup, unquote)
 from blockdiag.utils import XY
 
 
