bcbf3f1b957015be87d4b18a39722c903e36c88e
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2013-10-05 18:32:18 +0900

    Remove TODO.txt

--- src/actdiag_sphinxhelper.py
+++ src/actdiag_sphinxhelper.py
@@ -18,19 +18,15 @@ import actdiag.builder
 import actdiag.drawer
 core = actdiag
 
+import actdiag.utils.rst.nodes
+import actdiag.utils.rst.directives
+utils = actdiag.utils
+
 import blockdiag.utils.bootstrap
-import blockdiag.utils.collections
+utils.bootstrap = blockdiag.utils.bootstrap
+
+import blockdiag.utils.compat
+utils.compat = blockdiag.utils.compat
+
 import blockdiag.utils.fontmap
-utils = blockdiag.utils
-
-from actdiag.utils.rst import nodes
-from actdiag.utils.rst import directives
-
-# FIXME: obsoleted interface (keep for compatibility)
-from blockdiag.utils import collections
-from blockdiag.utils.fontmap import FontMap
-from actdiag import command, parser, builder, drawer
-from actdiag.utils.rst.directives import actdiag, ActdiagDirective
-
-(collections, FontMap, command, parser, builder, drawer,
- nodes, directives, actdiag, ActdiagDirective)
+utils.fontmap = blockdiag.utils.fontmap
