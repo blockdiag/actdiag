7d37047d7a7d3f779feb041f05bd303e6f3ebcb8
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2013-10-11 12:16:30 +0900

    Fix flake8 violations

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
 from setuptools import setup, find_packages
-import os, sys
 
 sys.path.insert(0, 'src')
 import actdiag
 
-long_description = open(os.path.join("src","README.txt")).read()
+long_description = open(os.path.join("src", "README.txt")).read()
 
 classifiers = [
     "Development Status :: 3 - Alpha",
@@ -17,50 +18,47 @@ classifiers = [
     "Topic :: Text Processing :: Markup",
 ]
 
-test_requires = [
-             'Nose',
-             'pep8>=1.3',]
+test_requires = ['Nose', 'pep8>=1.3']
 
 # only for Python2.6
 if sys.version_info > (2, 6) and sys.version_info < (2, 7):
     test_requires.append('unittest2')
 
 setup(
-     name='actdiag',
-     version=actdiag.__version__,
-     description='actdiag generate activity-diagram image file from spec-text file.',
-     long_description=long_description,
-     classifiers=classifiers,
-     keywords=['diagram','generator'],
-     author='Takeshi Komiya',
-     author_email='i.tkomiya at gmail.com',
-     url='http://blockdiag.com/',
-     download_url='http://pypi.python.org/pypi/actdiag',
-     license='Apache License 2.0',
-     py_modules=['actdiag_sphinxhelper'],
-     packages=find_packages('src'),
-     package_dir={'': 'src'},
-     package_data = {'': ['buildout.cfg']},
-     include_package_data=True,
-     install_requires=[
+    name='actdiag',
+    version=actdiag.__version__,
+    description='actdiag generates activity-diagram image from text',
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords=['diagram', 'generator'],
+    author='Takeshi Komiya',
+    author_email='i.tkomiya at gmail.com',
+    url='http://blockdiag.com/',
+    download_url='http://pypi.python.org/pypi/actdiag',
+    license='Apache License 2.0',
+    py_modules=['actdiag_sphinxhelper'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={'': ['buildout.cfg']},
+    include_package_data=True,
+    install_requires=[
         'setuptools',
         'blockdiag>=1.3.0',
          # -*- Extra requirements: -*-
-     ],
-     extras_require=dict(
-         test=test_requires,
-         rst=[
-             'docutils',
-         ],
-     ),
-     test_suite='nose.collector',
-     tests_require=test_requires,
-     entry_points="""
-        [console_scripts]
-        actdiag = actdiag.command:main
+    ],
+    extras_require=dict(
+        test=test_requires,
+        rst=[
+            'docutils',
+        ],
+    ),
+    test_suite='nose.collector',
+    tests_require=test_requires,
+    entry_points="""
+       [console_scripts]
+       actdiag = actdiag.command:main
 
-        [blockdiag_plugins]
-        autolane = actdiag.plugins.autolane
-     """,
+       [blockdiag_plugins]
+       autolane = actdiag.plugins.autolane
+    """,
 )
-
