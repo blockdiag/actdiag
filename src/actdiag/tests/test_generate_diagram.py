26359841e62343b86b484ddd962b31ffe811feee
tk0miya <i.tkomiya@gmail.com>
2013-11-06 15:22:41 +0900

    Refactor test_generate_diagram

---
+++ src/actdiag/tests/test_generate_diagram.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import os
+import actdiag.command
+from blockdiag.tests.test_generate_diagram import (
+    get_diagram_files, testcase_generator
+)
+
+
+def test_generate():
+    mainfunc = actdiag.command.main
+    basepath = os.path.dirname(__file__)
+    files = get_diagram_files(basepath)
+    options = []
+
+    for testcase in testcase_generator(basepath, mainfunc, files, options):
+        yield testcase
