09c8defa54f629231602f37f4a6568f25cc9fee1
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2014-03-01 17:12:47 +0900

    Simplify tox.ini (refer extra_require[testing])

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ classifiers = [
     "Topic :: Text Processing :: Markup",
 ]
 
-test_requires = ['Nose', 'pep8>=1.3']
+test_requires = ['nose', 'pep8>=1.3', 'docutils']
 
 # only for Python2.6
 if sys.version_info > (2, 6) and sys.version_info < (2, 7):
@@ -44,7 +44,7 @@ setup(
          # -*- Extra requirements: -*-
     ],
     extras_require=dict(
-        test=test_requires,
+        testing=test_requires,
         rst=[
             'docutils',
         ],
