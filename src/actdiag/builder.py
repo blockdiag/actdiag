f4675012c6c84bd2ff416da4549b601510865a92
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2014-04-08 16:04:59 +0900

    Fix flake8 violations

--- src/actdiag/builder.py
+++ src/actdiag/builder.py
@@ -219,7 +219,7 @@ class DiagramLayoutManager:
                 parents = []
                 for node in circular:
                     for parent in self.get_parent_nodes(node):
-                        if not parent in circular:
+                        if parent not in circular:
                             parents.append(parent)
 
                 parents.sort(key=lambda x: x.order)
