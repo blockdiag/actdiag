ff4a447cda83531a88b131d0e71811188fffdc30
tk0miya <i.tkomiya@gmail.com>
2014-06-24 22:59:57 +0900

    Fix flake8 violations

--- src/actdiag/utils/rst/directives.py
+++ src/actdiag/utils/rst/directives.py
@@ -13,8 +13,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import os
-from docutils import nodes
 from docutils.parsers import rst
 import actdiag.parser
 import actdiag.builder
