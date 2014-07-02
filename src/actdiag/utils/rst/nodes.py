3095cdd9a6d678d2e871d251a19e5febbeaacc3a
tk0miya <i.tkomiya@gmail.com>
2014-07-02 15:28:57 +0900

    Add to_diagram(), to_drawer() and get_path() methods to rst.nodes.actdiag

--- src/actdiag/utils/rst/nodes.py
+++ src/actdiag/utils/rst/nodes.py
@@ -13,8 +13,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from docutils import nodes
+import actdiag.parser
+import actdiag.builder
+import actdiag.drawer
+from blockdiag.utils.rst import nodes
 
 
-class actdiag(nodes.General, nodes.Element):
-    pass
+class actdiag(nodes.blockdiag):
+    name = 'actdiag'
+    processor = actdiag
