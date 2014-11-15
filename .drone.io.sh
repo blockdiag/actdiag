014c4a3049cb52d8dc207038ef0258c550010487
tk0miya <i.tkomiya@gmail.com>
2014-11-15 14:49:31 +0900

    Improve test skipper

--- .drone.io.sh
+++ .drone.io.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
-sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev fonts-ipafont-gothic fonts-vlgothic libjpeg8-dev libfreetype6-dev
-
-mkdir src/actdiag/tests/truetype
-cp /usr/share/fonts/truetype/vlgothic/VL-PGothic-Regular.ttf src/actdiag/tests/truetype
+sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev fonts-ipafont-gothic libjpeg8-dev libfreetype6-dev
 
 pip install --use-mirrors --upgrade detox misspellings check-manifest docutils
 find src/ -name "*.py" | misspellings -f -
